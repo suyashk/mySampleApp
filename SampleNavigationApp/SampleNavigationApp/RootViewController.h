ba4d01155bbe3d150e342dcf519f1ab530172e02
Suyash Kaulgud <sukaulgud@deloitte.com>
2011-05-19 13:25:26 +0530

    adding uodated code

---
+++ SampleNavigationApp/SampleNavigationApp/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SampleNavigationApp
+//
+//  Created by Deloitte-1 on 5/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ SampleNavigationApp/SampleNavigationApp/SampleNavigationAppAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SampleNavigationAppAppDelegate.h
+//  SampleNavigationApp
+//
+//  Created by Deloitte-1 on 5/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleNavigationAppAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
